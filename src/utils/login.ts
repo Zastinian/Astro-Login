59e8bf8fa4310f8aa828055af20db3415e7afd48
Zastinian <zastinian@gmail.com>
2024-03-14 17:46:50 -0700

    feat: Env Variables

--- src/utils/login.ts
+++ src/utils/login.ts
@@ -1,14 +1,13 @@
 import wbtl from "jsonwebtoken";
 import {dbLogin} from "./routes";
 
-export default async function login(email: string, password: string) {
+export default async function login(email: string, password: string, maxAge: number) {
   const getLoginData = (await dbLogin(email, password).then((data) => {
     return data;
   })) as any;
   if (getLoginData.success == false) {
     return {success: false, token: null, max: null};
   } else {
-    const maxAge = 60 * 60 * 24 * 7;
     const expire = Math.floor(Date.now() / 1000) + maxAge;
     const token = wbtl.sign(
       {
