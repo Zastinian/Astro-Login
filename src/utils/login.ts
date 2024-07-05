1f10848919ee996b541b2c398aad7bb9433e6f78
Zastinian <zastinian@gmail.com>
2024-07-04 20:49:10 -0600

    feat: Added dbLogin types

--- src/utils/login.ts
+++ src/utils/login.ts
@@ -1,22 +1,21 @@
 import wbtl from "jsonwebtoken";
-import {dbLogin} from "./routes";
+import { dbLogin } from "./routes";
 
 export default async function login(email: string, password: string, maxAge: number) {
   const getLoginData = (await dbLogin(email, password).then((data) => {
     return data;
-  })) as any;
-  if (getLoginData.success == false) {
-    return {success: false, token: null, max: null};
-  } else {
-    const expire = Math.floor(Date.now() / 1000) + maxAge;
-    const token = wbtl.sign(
-      {
-        exp: expire,
-        email: email,
-        username: getLoginData.username,
-      },
-      "secret",
-    );
-    return {success: true, token: `${token}`, max: maxAge};
+  }));
+  if (getLoginData.success === false) {
+    return { success: false, token: null, max: null };
   }
+  const expire = Math.floor(Date.now() / 1000) + maxAge;
+  const token = wbtl.sign(
+    {
+      exp: expire,
+      email: email,
+      username: getLoginData.username,
+    },
+    "secret",
+  );
+  return { success: true, token: `${token}`, max: maxAge };
 }
--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,12 +1,17 @@
-export function dbLogin(email: string, password: string) {
+interface dbLoginType {
+  success: boolean;
+  username: string | null;
+}
+
+export function dbLogin(email: string, password: string): Promise<dbLoginType> {
   let username: string | null = null;
-  let success: boolean = true;
+  let success = true;
   return new Promise((resolve, reject) => {
     if (email === "user@example.com" && password === "1234") {
       username = "User Test";
     } else {
       success = false;
     }
-    resolve({success, username});
+    resolve({ success, username });
   });
 }
