092461c12e590847c9556386e5f4dbbca4210e44
Zastinian <zastinian@gmail.com>
2024-08-04 13:59:44 -0600

    feat: Added cookie secret

--- src/utils/login.ts
+++ src/utils/login.ts
@@ -1,21 +1,31 @@
 import wbtl from "jsonwebtoken";
 import { dbLogin } from "./routes";
 
-export default async function login(email: string, password: string, maxAge: number) {
-  const getLoginData = (await dbLogin(email, password).then((data) => {
-    return data;
-  }));
-  if (getLoginData.success === false) {
-    return { success: false, token: null, max: null };
-  }
-  const expire = Math.floor(Date.now() / 1000) + maxAge;
-  const token = wbtl.sign(
-    {
-      exp: expire,
-      email: email,
-      username: getLoginData.username,
-    },
-    "secret",
-  );
-  return { success: true, token: `${token}`, max: maxAge };
+export default async function login({
+	email,
+	password,
+	maxAge,
+	secret,
+}: {
+	email: string;
+	password: string;
+	maxAge: number;
+	secret: string;
+}) {
+	const getLoginData = await dbLogin(email, password).then((data) => {
+		return data;
+	});
+	if (getLoginData.success === false) {
+		return { success: false, token: null, max: null };
+	}
+	const expire = Math.floor(Date.now() / 1000) + maxAge;
+	const token = wbtl.sign(
+		{
+			exp: expire,
+			email: email,
+			username: getLoginData.username,
+		},
+		secret,
+	);
+	return { success: true, token: `${token}`, max: maxAge };
 }
