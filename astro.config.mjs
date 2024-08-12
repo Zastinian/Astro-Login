de003040c0d68d91e3ac8286e488786b358313f5
Zastinian <zastinian@gmail.com>
2024-08-12 10:37:41 -0600

    feat: Change cloudflare to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: "server",
-  adapter: cloudflare({
-    imageService: "passthrough",
-  }),
+  adapter: vercel(),
   server: {
     host: process.env.HOST || "localhost",
     port: process.env.PORT || 4321,
-  },
-  vite: {
-    ssr: {
-      external: ["crypto", "stream", "util", "buffer"]
-    }
   }
 });
