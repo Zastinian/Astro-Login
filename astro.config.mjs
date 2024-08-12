67405b9a1f1a56c79a219ea03ef498cc20c28a37
Zastinian <zastinian@gmail.com>
2024-08-12 10:00:33 -0600

    feat: Added plataformProxy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    }
+  }),
   vite: {
     ssr: {
       external: ["crypto", "stream", "util", "buffer"]
