164bcfbc8224f099099082c09dfe14b78e2c84de
Zastinian <zastinian@gmail.com>
2024-08-12 10:32:02 -0600

    feat: Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   output: "server",
   adapter: cloudflare({
-    imageService: "passthrough"
+    imageService: "passthrough",
   }),
+  server: {
+    host: process.env.HOST || "localhost",
+    port: process.env.PORT || 4321,
+  },
   vite: {
     ssr: {
       external: ["crypto", "stream", "util", "buffer"]
