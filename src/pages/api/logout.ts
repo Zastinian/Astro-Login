fd08287e9f035c5996305b3e2185c5aa0c4c9c91
Zastinian <zastinian@gmail.com>
2024-09-10 11:36:01 -0600

    feat: Added login & logout api

---
+++ src/pages/api/logout.ts
@@ -0,0 +1,13 @@
+import type { APIRoute } from "astro";
+
+const cookieName = import.meta.env.COOKIE_NAME || "_Security_Login_";
+
+export const POST: APIRoute = async ({ cookies, redirect }) => {
+	const cook: string = `${cookies.get(cookieName)?.value}`;
+	cookies.set(cookieName, cook, {
+		path: "/",
+		maxAge: 0,
+		secure: Boolean(import.meta.env.COOKIE_SECURE || false),
+	});
+	return redirect("/");
+};
