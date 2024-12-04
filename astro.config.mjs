90746e92c3b22ae14bc1a7554e90896f46efb422
Zastinian <zastinian@gmail.com>
2024-12-03 21:17:22 -0600

    feat: Removed serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: "server",
