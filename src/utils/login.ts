78431fbce666966521d6216bd13362be1820dd97
Zastinian <zastinian@gmail.com>
2025-01-08 12:50:19 -0600

    feat: Add conditional types for login response

--- src/utils/login.ts
+++ src/utils/login.ts
@@ -1,31 +1,33 @@
 import wbtl from "jsonwebtoken";
 import { dbLogin } from "./routes";
 
+type LoginResponse =
+  | { success: false; token: null; max: null }
+  | { success: true; token: string; max: number };
+
 export default async function login({
-	email,
-	password,
-	maxAge,
-	secret,
+  email,
+  password,
+  maxAge,
+  secret,
 }: {
-	email: string;
-	password: string;
-	maxAge: number;
-	secret: string;
-}) {
-	const getLoginData = await dbLogin(email, password).then((data) => {
-		return data;
-	});
-	if (getLoginData.success === false) {
-		return { success: false, token: null, max: null };
-	}
-	const expire = Math.floor(Date.now() / 1000) + maxAge;
-	const token = wbtl.sign(
-		{
-			exp: expire,
-			email: email,
-			username: getLoginData.username,
-		},
-		secret,
-	);
-	return { success: true, token: `${token}`, max: maxAge };
+  email: string;
+  password: string;
+  maxAge: number;
+  secret: string;
+}): Promise<LoginResponse> {
+  const getLoginData = await dbLogin(email, password);
+  if (getLoginData.success === false) {
+    return { success: false, token: null, max: null };
+  }
+  const expire = Math.floor(Date.now() / 1000) + maxAge;
+  const token = wbtl.sign(
+    {
+      exp: expire,
+      email: email,
+      username: getLoginData.username,
+    },
+    secret,
+  );
+  return { success: true, token: `${token}`, max: maxAge };
 }
