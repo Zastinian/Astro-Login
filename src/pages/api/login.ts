11e195f4ef52bc3e61984b409bd897b936c70a7a
Zastinian <zastinian@gmail.com>
2025-03-14 13:28:00 -0600

    refactor: Clean up code formatting

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -5,23 +5,23 @@ const cookieName = import.meta.env.COOKIE_NAME || "_Security_Login_";
 let maxAge = 604800;
 
 export const POST: APIRoute = async ({ cookies, redirect, request }) => {
-	const data = Object.fromEntries(new URLSearchParams(await request.text()));
-	const d = await loginSave({
-		email: data.email,
-		password: data.password,
-		maxAge: Number(import.meta.env.MAX_AGE || 604800),
-		secret: import.meta.env.COOKIE_SECRET || "secret",
-	});
-	let token = "none";
-	if (d.success === true) {
-		token = d?.token || "none";
-		maxAge = d?.max || 604800;
-		cookies.set(cookieName, token, {
-			path: "/",
-			maxAge: maxAge,
-			secure: Boolean(import.meta.env.COOKIE_SECURE || false),
-		});
-		return redirect("/");
-	}
-	return redirect("/login?error=true");
+  const data = Object.fromEntries(new URLSearchParams(await request.text()));
+  const d = await loginSave({
+    email: data.email,
+    password: data.password,
+    maxAge: Number(import.meta.env.MAX_AGE || 604800),
+    secret: import.meta.env.COOKIE_SECRET || "secret",
+  });
+  let token = "none";
+  if (d.success === true) {
+    token = d?.token || "none";
+    maxAge = d?.max || 604800;
+    cookies.set(cookieName, token, {
+      path: "/",
+      maxAge: maxAge,
+      secure: Boolean(import.meta.env.COOKIE_SECURE || false),
+    });
+    return redirect("/");
+  }
+  return redirect("/login?error=true");
 };
--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -3,11 +3,11 @@ import type { APIRoute } from "astro";
 const cookieName = import.meta.env.COOKIE_NAME || "_Security_Login_";
 
 export const GET: APIRoute = async ({ cookies, redirect }) => {
-	const cook: string = `${cookies.get(cookieName)?.value}`;
-	cookies.set(cookieName, cook, {
-		path: "/",
-		maxAge: 0,
-		secure: Boolean(import.meta.env.COOKIE_SECURE || false),
-	});
-	return redirect("/");
+  const cook: string = `${cookies.get(cookieName)?.value}`;
+  cookies.set(cookieName, cook, {
+    path: "/",
+    maxAge: 0,
+    secure: Boolean(import.meta.env.COOKIE_SECURE || false),
+  });
+  return redirect("/");
 };
